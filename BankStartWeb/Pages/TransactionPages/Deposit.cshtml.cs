4334b44d3ff9a5eefda92c0b79373b8f31af08a1
Per görtz <pelle_gortz@hotmail.com>
2022-04-10 08:49:00 +0200

    Made some validation for withdrawals, and added some style cards for transaction actions

--- BankStartWeb/Pages/TransactionPages/Deposit.cshtml.cs
+++ BankStartWeb/Pages/TransactionPages/Deposit.cshtml.cs
@@ -11,26 +11,27 @@ namespace BankStartWeb.Pages.Transactions
     public class DepositModel : PageModel
     {
         private readonly ApplicationDbContext _context;
-        
+
+        public DepositModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public string Operation { get; set; }
         public string Type { get; set; }
         public decimal Amount { get; set; }
+        public int CustomerId { get; set; }
         public int AccountId { get; set; }
         public Account Account { get; set; }
         public Customer Customer { get; set; }
         public List<SelectListItem> AllTypes { get; set; }
         public List<SelectListItem> AllOperations { get; set; }
-        public int CustomerId { get; set; }
-        public DepositModel(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+
         public void OnGet(int accountId, int customerId)
         {
             AccountId = accountId;
             CustomerId = customerId;
-            SetTypes();
-            SetOperations();
+            SetSelectLists();
         }
 
         public IActionResult OnPost(int accountId, int customerId)
@@ -40,62 +41,40 @@ namespace BankStartWeb.Pages.Transactions
                 Customer = _context.Customers.First(c => c.Id == customerId);
                 Account = _context.Accounts.Include(t => t.Transactions).First(a => a.Id == accountId);
                 Account.Transactions.Add(new Transaction
-                {   
+                {
                     Amount = Amount,
                     Operation = Operation,
-                    Date= DateTime.Now,
-                    Type= Type,
-                    NewBalance = Account.Balance+Amount
+                    Date = DateTime.Now,
+                    Type = Type,
+                    NewBalance = Account.Balance + Amount
                 });
                 Account.Balance = Account.Balance + Amount;
                 _context.SaveChanges();
             }
 
-            return RedirectToPage("/CustomerPages/Customer", new { customerId });
+            return RedirectToPage("/CustomerPages/Customer", new {customerId});
         }
-
+        public void SetSelectLists()
+        {
+            SetTypes();
+            SetOperations();
+        }
         private void SetTypes()
         {
             AllTypes = new List<SelectListItem>()
             {
-                new SelectListItem
-                {
-                    Text = "Debit",
-                    Value = "Debit"
-                },
-                new SelectListItem
-                {
-                    Text = "Credit",
-                    Value = "Credit"
-                }
+                new SelectListItem {Text = "Debit", Value = "Debit"},
+                new SelectListItem {Text = "Credit", Value = "Credit"}
             };
         }
+
         private void SetOperations()
         {
             AllOperations = new List<SelectListItem>()
             {
-                new SelectListItem
-                {
-                    Text = "Deposit Cash",
-                    Value = "Deposit cash"
-                },
-                new SelectListItem
-                {
-                    Text = "Salary",
-                    Value = "Salary"
-                },
-                new SelectListItem
-                {
-                    Text = "Transfer",
-                    Value = "Transfer"
-                },
-                new SelectListItem
-                {
-                    Text = "Payment",
-                    Value = "Payment"
-                },
+                new SelectListItem {Text = "Deposit Cash", Value = "Deposit cash"},
+                
             };
         }
-
     }
 }
--- BankStartWeb/Pages/TransactionPages/Withdrawal.cshtml.cs
+++ BankStartWeb/Pages/TransactionPages/Withdrawal.cshtml.cs
@@ -4,95 +4,99 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-namespace BankStartWeb.Pages.Transactions
-{[BindProperties]
-    public class WithdrawalModel : PageModel
+namespace BankStartWeb.Pages.Transactions;
+
+[BindProperties]
+public class WithdrawalModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public WithdrawalModel(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-        public int AccountId { get; set; }
-        public int CustomerId { get; set; }
-        public string Operation { get; set; }
-        public string Type { get; set; }
-        public decimal Amount { get; set; }
-        public bool ValidWithdrawal { get; set; }
-        public Account Account { get; set; }
-        public Customer Customer { get; set; }
-        public List<SelectListItem> AllTypes { get; set; }
-        public List<SelectListItem> AllOperations { get; set; }
+        _context = context;
+    }
 
-        public WithdrawalModel(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public int AccountId { get; set; }
+    public int CustomerId { get; set; }
+    public string Operation { get; set; }
+    public string Type { get; set; }
+    public decimal Amount { get; set; }
+    public Account Account { get; set; }
+    public Customer Customer { get; set; }
+    public List<SelectListItem> AllTypes { get; set; }
+    public List<SelectListItem> AllOperations { get; set; }
 
-        public void OnGet(int accountId, int customerId)
+    public void OnGet(int accountId, int customerId)
+    {
+        AccountId = accountId;
+        CustomerId = customerId;
+        SetSelectLists();
+    }
+    public IActionResult OnPost(int accountId, int customerId)
+    {
+        if (ModelState.IsValid)
         {
-            AccountId = accountId;
-            CustomerId = customerId;
-            SetTypes();
-            SetOperations();
-        }
-
-        //public bool IsValidWithDrawal()
-        //{
-        //    Account.Balance < Account.
-        //}
-        public IActionResult OnPost(int accountId, int customerId)
-        {
-            if (ModelState.IsValid)
+            Customer = _context.Customers.First(c => c.Id == customerId);
+            Account = _context.Accounts.Include(t => t.Transactions).First(a => a.Id == accountId);
+            var withdrawal = new Transaction
             {
-                Customer = _context.Customers.First(c => c.Id == customerId);
-                Account = _context.Accounts.Include(t => t.Transactions).First(a => a.Id == accountId);
-                Account.Transactions.Add(new Transaction
-                {
-                    Amount = Amount,
-                    Operation = Operation,
-                    Date = DateTime.Now,
-                    Type = Type,
-                    NewBalance = Account.Balance - Amount
-                });
-                Account.Balance = Account.Balance - Amount;
-                _context.SaveChanges();
+                Amount = Amount,
+                Operation = Operation,
+                Date = DateTime.Now,
+                Type = Type,
+                NewBalance = Account.Balance - Amount
+            };
+            if (withdrawal.NewBalance < 0)
+            {
+                ModelState.AddModelError(nameof(Amount),
+                    "There is not enough money for withdrawal, please select a lesser amount.");
+                SetSelectLists();
+                return Page();
             }
 
-            return RedirectToPage("/CustomerPages/Customer", new { customerId });
+            Account.Transactions.Add(withdrawal);
+            _context.SaveChanges();
         }
 
-        private void SetTypes()
-        {
-            AllTypes = new List<SelectListItem>()
-            {
-                new SelectListItem
-                {
-                    Text = "Debit",
-                    Value = "Debit"
-                },
-                new SelectListItem
-                {
-                    Text = "Credit",
-                    Value = "Credit"
-                }
-            };
-        }
+        return RedirectToPage("/CustomerPages/Customer", new { customerId });
+    }
 
-        private void SetOperations()
+    public void SetSelectLists()
+    {
+        SetTypes();
+        SetOperations();
+    }
+    private void SetTypes()
+    {
+        AllTypes = new List<SelectListItem>
         {
-            AllOperations = new List<SelectListItem>()
+            new()
             {
-                
-                new SelectListItem
-                {
-                    Text = "Transfer",
-                    Value = "Transfer"
-                },
-                new SelectListItem
-                {
-                    Text = "ATM withdrawal",
-                    Value = "ATM withdrawal"
-                },
-                
-            };
-        }
+                Text = "Debit",
+                Value = "Debit"
+            },
+            new()
+            {
+                Text = "Credit",
+                Value = "Credit"
+            }
+        };
+    }
+
+    private void SetOperations()
+    {
+        AllOperations = new List<SelectListItem>
+        {
+            new()
+            {
+                Text = "Transfer",
+                Value = "Transfer"
+            },
+            new()
+            {
+                Text = "ATM withdrawal",
+                Value = "ATM withdrawal"
+            }
+        };
     }
 }
-
